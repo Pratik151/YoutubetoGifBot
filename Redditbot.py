fff8f0d177839be7ba3e6b99dd85bb42d84e8e49
Pratik151 <pratikpatel15133@gmail.com>
2015-06-09 14:46:19 +0530

    Updated/Changed
    Changed a lots of things. Soon will make good documentation of changes.

--- Redditbot.py
+++ Redditbot.py
@@ -6,49 +6,42 @@ import time
 
 user_agent = ("Reddit Youtube to gif v0.1 by /u/Austinto")
 r = praw.Reddit(user_agent=user_agent)
+r.login("YoutubeToGifBot","pratik151")
 
-#Replace with your reddit username and password of bot.
-r.login("REDDIT_USERNAME","REDDIT_PASSWORD")
-wordsToMatch = ['!gfycatbot']
-cache = []
 
-#used to find the length of the video.
+
 def get_length(url):
     video = pafy.new(url)
     length=video.length
     return length
-    
-#used to convert video to gif.
+
+
+
 def convert_to_gif(url):
     response = urllib2.urlopen(url)
+    time.sleep(100)    
+    response = urllib2.urlopen(url)
     data = json.load(response)
-    return data['gifUrl']
+    gifurl = data['gifUrl']
+    gifurl = gifurl.replace("http://giant.","")
+    gifurl = gifurl.replace(".gif","")
+    gifurl = "http://"+gifurl
+    return gifurl
+
 
 def run_bot():
-    subreddit = r.get_subreddit("SUBREDDITNAME")
-    for submission in subreddit.get_hot(limit=1):
-        comments = submission.comments
-        for comment in comments:
-            text = comment.body
-            isMatch = any(string in text for string in wordsToMatch)
-            
-            if isMatch:
-                url = submission.url
-                if "youtu" in url:
-                    if get_length(url) <= 15:
-                        url1 = "http://upload.gfycat.com/transcode?fetchUrl="+url+"&fetchSeconds=0&fetchMinutes=0&fetchHours=0&fetchLength="+str(get_length(url))
-                        gifurl = convert_to_gif(url1)
-                        if comment.id not in cache:
-                            comment.reply("Here is the Gif Version of the video : "+gifurl+"\nThis is a bot and won't answer to messages. Message to the [Owner](http://www.reddit.com/message/compose/?to=Austinto&amp;subject=BotReport) for bug reports.")
-                            cache.append(comment.id)
-                    else:
-                        comment.reply("Length of the video should be less than 15sec.\nThis is a bot and won't answer to messages. Message to the [Owner](http://www.reddit.com/message/compose/?to=Austinto&amp;subject=BotReport)")
-                        cache.append(comment.id)
+    subreddit = r.get_subreddit("leagueoflegends")
+    for submission in subreddit.get_rising(limit=30):
+        url = submission.url
+        if "youtu" in url:
+            if get_length(url) <= 15:
+                if submission.id not in open('Reddit','r').read().split(','):
+                    url1 = "http://upload.gfycat.com/transcode?fetchUrl="+url+"&fetchSeconds=0&fetchMinutes=0&fetchHours=0&fetchLength="+str(get_length(url))
+                    gifurl = convert_to_gif(url1)
+                    submission.add_comment("Here is the Gif Version of the video : "+gifurl+"."+"\n\n---------------------------------------------------\n\nThis is a bot and won't answer to messages. Message to the [Owner](http://www.reddit.com/message/compose/?to=Austinto&amp;subject=BotReport) for bug reports.")
+                    open('Reddit','a').write(','+submission.id)
 
 while True:
-    run_bot()
-    time.sleep(10)
-    
-
-
+    run_bot();
+    time.sleep(5)
 
